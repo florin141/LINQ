b2550bf808392e3072be4bc720d12cb713d9028a
cflorin <cflorin@CFLORIN-LPT>
2019-10-02 23:11:09 +0200

    Writing report with and without LINQ

--- Introduction/Properties/AssemblyInfo.cs
+++ Introduction/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("Introduction")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("Introduction")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55c4340d-b26c-4225-a8a5-7f6c941baa94")]
+[assembly: Guid("7257e4d8-b518-455e-a370-c0d6c5e8e770")]
 
 // Version information for an assembly consists of the following four values:
 //
