50216bc3ef54d9a8ac0a764bcb49967135fd26d7
cflorin <cflorin@CFLORIN-LPT>
2019-10-06 17:49:43 +0200

    Filtering with Where and First

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -14,25 +14,30 @@ namespace Cars
             var cars = ProcessFile("fuel.csv");
 
             var query1 = cars
+                .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
                 .OrderByDescending(c => c.Combined)
-                .ThenBy(c => c.Name);
+                .ThenBy(c => c.Name)
+                .First();
 
             var query2 = from car in cars
-                         orderby car.Combined ascending, car.Name ascending
+                         where car.Manufacturer == "BMW" && car.Year == 2016
+                         orderby car.Combined, car.Name
                          select car;
 
-            foreach (var car in query2.Take(10))
-            {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
-            }
+            Console.WriteLine($"{query1.Name} : {query1.Combined}");
+
+            //foreach (var car in query1.Take(10))
+            //{
+            //    Console.WriteLine($"{car.Name} : {car.Combined}");
+            //}
         }
 
         private static List<Car> ProcessFile(string path)
         {
             // Query Syntax
             var query1 = from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+                         where line.Length > 1
+                         select Car.ParseFromCsv(line);
 
             // Extension Method Syntax
             var query2 = File.ReadAllLines(path)
