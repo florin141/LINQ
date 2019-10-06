ced5a0464776f9a3c507f4505423a748003063e0
cflorin <cflorin@CFLORIN-LPT>
2019-10-06 13:58:27 +0200

    Implement a file processor

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace Cars
     {
         static void Main(string[] args)
         {
+            var cars = ProcessFile("fuel.csv");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            // Query Syntax
+            var query1 = from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            // Extension Method Syntax
+            var query2 = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv);
+
+            return query1.ToList();
         }
     }
 }
