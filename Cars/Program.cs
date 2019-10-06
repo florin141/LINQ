b430dfa9602bf413129f6ee5d8a3c9dfb2c6546e
cflorin <cflorin@CFLORIN-LPT>
2019-10-06 18:30:24 +0200

    Projecting data with Select

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,31 +13,34 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            var query1 = cars.All(c => c.Manufacturer == "Ford");
+            var query = from car in cars
+                where car.Manufacturer == "BMW" && car.Year == 2016
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    car.Manufacturer,
+                    car.Name,
+                    car.Combined
+                };
 
-            Console.WriteLine(cars.Any(c => c.Manufacturer == "Ford"));
-            Console.WriteLine(cars.All(c => c.Manufacturer == "Ford"));
+            var result = cars.Any(c => c.Manufacturer == "Ford");
 
-            var query2 = from car in cars
-                         where car.Manufacturer == "BMW" && car.Year == 2016
-                         orderby car.Combined, car.Name
-                         select car;
+            Console.WriteLine(result);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            // Query Syntax
-            var query1 = from line in File.ReadAllLines(path).Skip(1)
-                         where line.Length > 1
-                         select Car.ParseFromCsv(line);
-
-            // Extension Method Syntax
-            var query2 = File.ReadAllLines(path)
+            var query = File.ReadAllLines(path)
                 .Skip(1)
                 .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv);
+                .ToCar();
 
-            return query1.ToList();
+            return query.ToList();
         }
     }
 }
